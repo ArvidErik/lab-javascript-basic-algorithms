172f05b030afee856ee739b00987ad750c1a3bd5
ArvidErik <erik.arvid.hosszu@gmail.com>
2023-07-12 15:44:49 +0200

    1st iteration done

--- index.js
+++ index.js
@@ -1,7 +1,22 @@
 // Iteration 1: Names and Input
+// 1.1 Create a variable hacker1 with the driver's name.
+// 1.2 Print "The driver's name is XXXX".
+// 1.3 Create a variable hacker2 with the navigator's name.
+// 1.4 Print "The navigator's name is YYYY".
 
+//1.1
+let hacker1 = "Erik";
+//1.2
+console.log(hacker1);
+//1.3
+let hacker2 = "Arvid";
+//1-4
+console.log(hacker2);
 
 // Iteration 2: Conditionals
+
+
+
 
 
 // Iteration 3: Loops
